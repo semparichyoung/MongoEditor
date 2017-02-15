3a69b37f02005ec1de32e8955df7f39bedfd78f3
semparichyoung <semparichyoung@gmail.com>
2017-02-15 18:18:06 +0800

    fix mongo id and date will return empty array when PHP version under 5.3

--- lib/getDocuments.php
+++ lib/getDocuments.php
@@ -24,52 +24,53 @@ foreach ($res as $v) {
 		if(gettype($val) == "object") {
 			if(method_exists($val, "__toString")) {
 				if($k == "_id") {
-					// $v["fieldMongoDataType"] = "key";
 					$resAry["type"][$k] = "key";
-					// $val = $val -> __toString();
+					$val = ['$oid' => (string)$val];
 				}else if(get_class($val) == "MongoDB\BSON\UTCDateTime") {
-					// $v["fieldMongoDataType"] = "date";
 					$resAry["type"][$k] = "date";
-					// $val = getDateByFormat($val);
+					$val = ['$date' => ['$numberLong' => (string)$val]];
 				}else {
 					$resAry["type"][$k] = $val -> __toString();
-					// $val = $val -> __toString();
 				}
 			} else {
-				// $v["fieldMongoDataType"] = "array";
-				$tmp = (array)$val;
-				// if(count($resAry["type"][$k]) <= count($tmp)) {
-					if(isAssoc($tmp)) {
-						// $resAry["type"][$k] = [];
-						foreach ($tmp as $key => $value) {
-							if(get_class($value) == "MongoDB\BSON\ObjectID") {
-								$resAry["type"][$k][$key] = "_id";
-							}else if(get_class($value) == "MongoDB\BSON\UTCDateTime") {
-								$resAry["type"][$k][$key] = "date";
-							}else {
-								$resAry["type"][$k][$key] = gettype($value);
-							}
-						}
-					}else {
-						if(get_class($tmp[0]) == "MongoDB\BSON\ObjectID") {
-							$resAry["type"][$k] = "_id";
-						}else if(get_class($tmp[0]) == "MongoDB\BSON\UTCDateTime") {
-							$resAry["type"][$k] = "date";
-						}else {
-							$resAry["type"][$k] = gettype($tmp[0]) . "/" . get_class($tmp[0]);
-						}
-					}
-				// }
+				foreach ($val as $key => &$value) {
+					fixObject($value, $resAry["type"][$k][$key]);
+
+					// if(get_class($value) == "MongoDB\BSON\ObjectID") {
+					// 	$resAry["type"][$k][$key] = "_id";
+					// 	$value = ['$oid' => (string)$value];
+					// }else if(get_class($value) == "MongoDB\BSON\UTCDateTime") {
+					// 	$resAry["type"][$k][$key] = "date";
+					// 	$value = ['$date' => ['$numberLong' => (string)$value]];
+					// }else {
+					// 	$resAry["type"][$k][$key] = gettype($value);
+					// }
+					
+				}
 			}
 		}else {
-			// $v["fieldMongoDataType"] = gettype($val);
 			if(array_search($k, $resAry["type"]) === false) {
 				$resAry["type"][$k] = gettype($val);
 			}
 		}
 	}
 	$resAry["data"][] = $v;
+	// var_dump(json_encode($v, JSON_FORCE_OBJECT));
 }
-
-
+function fixObject(&$value, &$type) {
+	if(get_class($value) == "MongoDB\BSON\ObjectID") {
+		$type = "_id";
+		$value = ['$oid' => (string)$value];
+	}else if(get_class($value) == "MongoDB\BSON\UTCDateTime") {
+		$type = "date";
+		$value = ['$date' => ['$numberLong' => (string)$value]];
+	}else if(gettype($value) == "object" && !method_exists($val, "__toString")){
+		foreach ($value as $k => &$v) {
+			fixObject($v, $type[$k]);
+		}
+	}else {
+		$type = gettype($value);
+	}
+}
+// var_dump($res);
 echo json_encode($resAry);
